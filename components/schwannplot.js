e3e2a436d8392d3197fdc6e61700b55afc318029
Ziyad Hameed <ziyadhameed@schwannlabs.bio>
2024-05-26 14:56:19 +0530

    Add * topics handling to the main rust thread, Added tracking of various models and make the module popup dynamic.

---
+++ components/schwannplot.js
@@ -0,0 +1,14 @@
+export default function Schwannplot(props) {
+    return (
+        <div>
+            <iframe
+                src="/schwann-plots.html"
+                style={{
+                    width: '100%',
+                    height: '100vh',
+                    border: 'none',
+                }}
+            ></iframe>
+        </div>
+    )
+}
